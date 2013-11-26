9b9b765211dbbd6e1c3aab57e926511beec2d53e
Jonas Swiatek <jals@dr.dk>
2013-11-26 16:27:17 +0100

    Added a helper class that searches a directory and sub directories for package.json files

---
+++ project/nodenpm/Npm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using npm_grunt;
+
+namespace NodeNpm
+{
+    public static class NpmPackageSearch
+    {
+        public static IEnumerable<NpmApi> FindNpmApis(string workingDirectory)
+        {
+            const string fileToFind = "package.json";
+            const string skipIn = "node_modules";
+            var files = Directory.GetFiles(workingDirectory, fileToFind, SearchOption.AllDirectories).Where(a => !a.Contains(skipIn));
+
+            foreach (var file in files)
+            {
+                var directory = Path.GetDirectoryName(file);
+                yield return new NpmApi(new AutoNpmFactory(), directory, null);
+            }
+        }
+    }
+}
